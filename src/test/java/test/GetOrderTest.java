750ff8ba9f271e58fbd25e3c43ae9a41c080d84a
domost714 <dominik.ostrzechowski@interia.pl>
2023-06-08 13:53:55 +0200

    add new test for GetOrderTest

--- src/test/java/test/GetOrderTest.java
+++ src/test/java/test/GetOrderTest.java
@@ -5,8 +5,11 @@ import conf.EndpointManager;
 import conf.RequestHelper;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.lessThan;
 
 public class GetOrderTest {
     @Test
@@ -16,5 +19,13 @@ public class GetOrderTest {
         given().auth().oauth2(DataHelper.token)
                 .get(EndpointManager.orders + id)
                 .then().statusCode(200).body(containsString("Testerski"));
+    }
+    @Test
+    public void verifyPerformanceWhenPostOrderThenGetItsDetailsByIDTest() {
+        String id = RequestHelper.postOrderAndGetItsId(1, "Testerski");
+
+        given().auth().oauth2(DataHelper.token)
+                .get(EndpointManager.orders + id)
+                .then().statusCode(200).time(lessThan(3L), TimeUnit.SECONDS);
     }
 }
