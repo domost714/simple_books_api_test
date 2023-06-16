a913f21b928712be2402c3d8c2f285062e061586
domost714 <dominik.ostrzechowski@interia.pl>
2023-06-16 20:41:15 +0200

    add info to checkIfTokenIsValidTest and rename tests in GenerateTokenTests

--- src/test/java/test/GenerateTokenTests.java
+++ src/test/java/test/GenerateTokenTests.java
@@ -11,13 +11,14 @@ import static org.hamcrest.Matchers.lessThan;
 
 public class GenerateTokenTests {
     @Test
-    public void checkIfTokenIsValid() {
+    public void checkIfTokenIsValidTest() {
         given().body(DataHelper.generateTokenData).contentType("application/json")
                 .post(EndpointManager.token)
                 .then().statusCode(409);
+        System.out.println("If test failed, you have to change token variable in DataHelper class according to API documentation");
     }
     @Test
-    public void verifyPerformanceWhenGenerateToken() {
+    public void verifyPerformanceWhenGenerateTokenTest() {
 
         given()
                 .body(DataHelper.generateTokenData).contentType("application/json")
